d7e873689aef12aca7e9e4b3cd48bc6ee04be4f7
Jessica Kincaid <4j.kincaid@gmail.com>
2022-05-20 11:14:33 -0500

    practice php strings and integers

---
+++ PHP_Basics/hello.php
@@ -0,0 +1,14 @@
+<?php
+
+/* This is a doc block.
+  * It gives multiple lines of comments
+  * like a Python docstring.
+  * @author: Jessica Kincaid
+*/
+// our first php comment
+echo 'Hello World!'; // done
+echo 'Hello Jessica!'; // done
+/* This is 
+a multiline comment. 
+*/
+?>
---
+++ PHP_Basics/numbers.php
@@ -0,0 +1,13 @@
+<?php
+$num_1 = 0;
+$num_2 = 1;
+$num_3 = 2;
+
+// var_dump provides data type
+
+var_dump($num_1);
+var_dump(0);
+var_dump('0');
+
+var_dump($num_2 + $num_3 - $num_2);
+?>
