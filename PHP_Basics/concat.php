f7e591d0428028ff70ed677449903f737c283891
Jessica Kincaid <4j.kincaid@gmail.com>
2022-05-22 16:02:05 -0500

    review comparison operators

--- PHP_Basics/concat.php
+++ PHP_Basics/concat.php
@@ -5,22 +5,10 @@
   * @author: Jessica Kincaid
 */
 
-$name = 'Jessica';
-$string_one = "Learning to display \"Hello  $name!\" to the screen.\n";
-
-$string_one = 'Learning to display "Hello ' . $name . '!" to the screen.' . "\n";
-
-/* Concatenation guidelines
-  * Use "" for a single variable
-  * Use . when concatenating in a single line
-  * Use .= when concatenating multiple lines
-*/
-$string_one = 'Learning to display ';
-$string_one .= '"Hello ';
-$string_one .= $name;
-$string_one .= '!" to the screen.';
-$string_one .= "\n";
-// prepend to a string
-$string_one = 'I am ' . $string_one;
-echo $string_one;
+$a = 5;
+//Place your code below this comment
+$isBoolean = true;
+$isIdentical = false;
+$isIdentical = var_dump($a == "5");
+echo $isIdentical;
 ?>
---
+++ PHP_Basics/conditionals.php
@@ -0,0 +1,25 @@
+<!-- <?php
+$name = 'Jessica';
+
+$string_one = 'Learning to display ';
+$string_one .= '"Hello ';
+$string_one .= $name;
+$string_one .= '!" to the screen.';
+// $string_one = $string_one . "\n";
+// prepend to a string
+// $string_one = 'I am ' . $string_one;
+echo $string_one;
+
+var_dump(1 + "2");
+$a = 10;
+$b = "10";
+// var_dump($a == $b);
+// var_dump($a === $b);
+// var_dump($string_one == 'Learning to display "Hello Jessica" to the screen.');
+
+if ($string_one == 'Learning to display "Hello Jessica" to the screen.') {
+    echo 'the values match.';
+}
+?> 
+
+
